191a52832fa567f53823bd4c024b8d797e332a0c
ellij <elli.jokinen@gmail.com>
2021-01-23 15:23:01 +0200

    T1 Calculator valmis

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+  const [result, setResult] = useState('');
+  
+  const add = () =>{
+    setResult(parseInt(text1) + parseInt(text2));
+  }
+
+  const subtract = () =>{
+    setResult(parseInt(text1) - parseInt(text2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Result: {result}</Text>
+      <TextInput style={styles.textInput}
+        onChangeText={text1 => setText1(text1)} 
+        value={text1}
+        keyboardType={'numeric'}
+      />
+      <TextInput style={styles.textInput}
+        onChangeText={text2 => setText2(text2)} 
+        value={text2}
+        keyboardType={'numeric'}
+      />
+      <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+        <Button color="#000" onPress={add} title="+" />
+        <Button color="#000" onPress={subtract} title="-" />
+      </View>     
     </View>
   );
 }
@@ -15,7 +39,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',  
   },
+  text: {
+    fontSize: 18,
+  },
+  textInput: {
+    height: 40, 
+    width: "50%", 
+    borderColor: '#808080', 
+    borderWidth: 2, 
+    margin: 5,
+    fontSize: 18,    
+  },  
 });
